e1fd4da260217ce70f42832d2a0397c1f8c4ba46
mnr1419 <mendanarayanarao86@gmail.com>
2020-04-11 07:44:50 +0530

    Updates

---
+++ precache-manifest.85c0bd18c3480872955b44eb906a317e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02dfb30af72ecfe8774dc86aaa7790",
+    "url": "/spring_ract_redux/index.html"
+  },
+  {
+    "revision": "1d6a8e5161ba0be742dc",
+    "url": "/spring_ract_redux/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bb39721bf9aa7283d20e",
+    "url": "/spring_ract_redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d6a8e5161ba0be742dc",
+    "url": "/spring_ract_redux/static/js/2.4e880a4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spring_ract_redux/static/js/2.4e880a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb39721bf9aa7283d20e",
+    "url": "/spring_ract_redux/static/js/main.6f59c8a2.chunk.js"
+  },
+  {
+    "revision": "2dead94fba3e957e35df",
+    "url": "/spring_ract_redux/static/js/runtime-main.decce709.js"
+  }
+]);
